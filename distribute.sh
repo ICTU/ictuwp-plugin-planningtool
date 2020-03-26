5ea477927306f5c12a058c858aed5654d8abe3da
Paul van Buuren <paul@wbvb.nl>
2020-03-26 15:37:11 +0100

    bestandslocaties bijgewerkt

--- distribute.sh
+++ distribute.sh
@@ -1,39 +1,41 @@
 
 
-# sh '/shared-paul-files/Webs/git-repos/Digitale-Overheid---WordPress-plugin-Planning-Tool/distribute.sh' &>/dev/null
+# sh '/Users/paul/shared-paul-files/Webs/git-repos/Digitale-Overheid---WordPress-plugin-Planning-Tool/distribute.sh' &>/dev/null
 
 # voor een update van de CMB2 bestanden:
-# sh '/shared-paul-files/Webs/git-repos/ICTU---Digitale-Overheid-WP---rijksvideoplugin/get_cmb2_files.sh' &>/dev/null
+# sh '/Users/paul/shared-paul-files/Webs/git-repos/ICTU---Digitale-Overheid-WP---rijksvideoplugin/get_cmb2_files.sh' &>/dev/null
 
 
-# cp '/Volumes/Macintosh HD/shared-paul-files/Webs/git-repos/ICTU---Digitale-Overheid-WP-theme/less/00-palet.less' '/shared-paul-files/Webs/git-repos/Digitale-Overheid---WordPress-plugin-Planning-Tool/less/palet-do.less';
+# cp '/Volumes/Macintosh HD/shared-paul-files/Webs/git-repos/ICTU---Digitale-Overheid-WP-theme/less/00-palet.less' '/Users/paul/shared-paul-files/Webs/git-repos/Digitale-Overheid---WordPress-plugin-Planning-Tool/less/palet-do.less';
 
 echo '----------------------------------------------------------------';
 echo 'Distribute DO planning tool plugin';
 
 # clear the log file
-> '/shared-paul-files/Webs/ICTU/Gebruiker Centraal/development/wp-content/debug.log'
+> '/Users/paul/shared-paul-files/Webs/ICTU/Gebruiker Centraal/development/wp-content/debug.log'
 
 # copy to temp dir
-rsync -r -a --delete '/shared-paul-files/Webs/git-repos/Digitale-Overheid---WordPress-plugin-Planning-Tool/' '/shared-paul-files/Webs/temp/'
+rsync -r -a --delete '/Users/paul/shared-paul-files/Webs/git-repos/Digitale-Overheid---WordPress-plugin-Planning-Tool/' '/Users/paul/shared-paul-files/Webs/temp/'
 
 # clean up temp dir
-rm -rf '/shared-paul-files/Webs/temp/.git/'
-rm '/shared-paul-files/Webs/temp/.gitignore'
-rm '/shared-paul-files/Webs/temp/config.codekit3'
-rm '/shared-paul-files/Webs/temp/distribute.sh'
-rm '/shared-paul-files/Webs/temp/README.md'
-rm '/shared-paul-files/Webs/temp/LICENSE'
+rm -rf '/Users/paul/shared-paul-files/Webs/temp/.git/'
+rm '/Users/paul/shared-paul-files/Webs/temp/.gitignore'
+rm '/Users/paul/shared-paul-files/Webs/temp/config.codekit3'
+rm '/Users/paul/shared-paul-files/Webs/temp/distribute.sh'
+rm '/Users/paul/shared-paul-files/Webs/temp/README.md'
+rm '/Users/paul/shared-paul-files/Webs/temp/LICENSE'
+rm '/Users/paul/shared-paul-files/Webs/temp/wijzigingen-planningstool.txt'
+
 
 
 # clean up temp dir
-rm -rf '/shared-paul-files/Webs/temp/.git/'
-rm '/shared-paul-files/Webs/temp/.gitignore'
-rm '/shared-paul-files/Webs/temp/config.codekit3'
-rm '/shared-paul-files/Webs/temp/.config.codekit3'
-rm '/shared-paul-files/Webs/temp/distribute.sh'
-rm '/shared-paul-files/Webs/temp/README.md'
-rm '/shared-paul-files/Webs/temp/LICENSE'
+rm -rf '/Users/paul/shared-paul-files/Webs/temp/.git/'
+rm '/Users/paul/shared-paul-files/Webs/temp/.gitignore'
+rm '/Users/paul/shared-paul-files/Webs/temp/config.codekit3'
+rm '/Users/paul/shared-paul-files/Webs/temp/.config.codekit3'
+rm '/Users/paul/shared-paul-files/Webs/temp/distribute.sh'
+rm '/Users/paul/shared-paul-files/Webs/temp/README.md'
+rm '/Users/paul/shared-paul-files/Webs/temp/LICENSE'
 
 
 
@@ -41,19 +43,19 @@ rm '/shared-paul-files/Webs/temp/LICENSE'
 # Vertalingen --------------------------------------------------------------------------------------------------------------------
 # --------------------------------------------------------------------------------------------------------------------------------
 # remove the .pot
-rm '/shared-paul-files/Webs/temp/languages/do-planning-tool.pot'
+rm '/Users/paul/shared-paul-files/Webs/temp/languages/do-planning-tool.pot'
 
 # copy files to /wp-content/languages/themes
-rsync -ah '/shared-paul-files/Webs/temp/languages/' '/shared-paul-files/Webs/ICTU/Gebruiker Centraal/development/wp-content/languages/plugins/'
+rsync -ah '/Users/paul/shared-paul-files/Webs/temp/languages/' '/Users/paul/shared-paul-files/Webs/ICTU/Gebruiker Centraal/development/wp-content/languages/plugins/'
 
 # languages Sentia accept
-rsync -ah '/shared-paul-files/Webs/temp/languages/' '/shared-paul-files/Webs/ICTU/Gebruiker Centraal/sentia/accept/www/wp-content/languages/plugins/'
+rsync -ah '/Users/paul/shared-paul-files/Webs/temp/languages/' '/Users/paul/shared-paul-files/Webs/ICTU/Gebruiker Centraal/sentia/accept/www/wp-content/languages/plugins/'
 
 # languages Sentia live
-rsync -ah '/shared-paul-files/Webs/temp/languages/' '/shared-paul-files/Webs/ICTU/Gebruiker Centraal/sentia/live/www/wp-content/languages/plugins/'
+rsync -ah '/Users/paul/shared-paul-files/Webs/temp/languages/' '/Users/paul/shared-paul-files/Webs/ICTU/Gebruiker Centraal/sentia/live/www/wp-content/languages/plugins/'
 
 
-cd '/shared-paul-files/Webs/temp/'
+cd '/Users/paul/shared-paul-files/Webs/temp/'
 find . -name ‘*.DS_Store’ -type f -delete
 
 
@@ -61,17 +63,17 @@ find . -name ‘*.DS_Store’ -type f -delete
 
 
 # copy from temp dir to dev-env
-rsync -r -a --delete '/shared-paul-files/Webs/temp/' '/shared-paul-files/Webs/ICTU/Gebruiker Centraal/development/wp-content/plugins/do-planning-tool/' 
+rsync -r -a --delete '/Users/paul/shared-paul-files/Webs/temp/' '/Users/paul/shared-paul-files/Webs/ICTU/Gebruiker Centraal/development/wp-content/plugins/ictuwp-plugin-planningtool/' 
 
 # remove temp dir
-rm -rf '/shared-paul-files/Webs/temp/'
-rm -rf '/shared-paul-files/Webs/temp-languages/'
+rm -rf '/Users/paul/shared-paul-files/Webs/temp/'
+rm -rf '/Users/paul/shared-paul-files/Webs/temp-languages/'
 
 # een kopietje naar Sentia accept
-rsync -r -a --delete '/shared-paul-files/Webs/ICTU/Gebruiker Centraal/development/wp-content/plugins/do-planning-tool/' '/shared-paul-files/Webs/ICTU/Gebruiker Centraal/sentia/accept/www/wp-content/plugins/do-planning-tool/'
+rsync -r -a --delete '/Users/paul/shared-paul-files/Webs/ICTU/Gebruiker Centraal/development/wp-content/plugins/ictuwp-plugin-planningtool/' '/Users/paul/shared-paul-files/Webs/ICTU/Gebruiker Centraal/sentia/accept/www/wp-content/plugins/ictuwp-plugin-planningtool/'
 
 # en een kopietje naar Sentia live
-rsync -r -a --delete '/shared-paul-files/Webs/ICTU/Gebruiker Centraal/development/wp-content/plugins/do-planning-tool/' '/shared-paul-files/Webs/ICTU/Gebruiker Centraal/sentia/live/www/wp-content/plugins/do-planning-tool/'
+rsync -r -a --delete '/Users/paul/shared-paul-files/Webs/ICTU/Gebruiker Centraal/development/wp-content/plugins/ictuwp-plugin-planningtool/' '/Users/paul/shared-paul-files/Webs/ICTU/Gebruiker Centraal/sentia/live/www/wp-content/plugins/ictuwp-plugin-planningtool/'
 
 
 
